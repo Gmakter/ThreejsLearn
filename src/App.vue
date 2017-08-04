71d181e1d74189282f9de808e837a4dcbfc256aa
gmakter <a845354046@qq.com>
2017-08-04 14:02:28 +0800

    初始化vue +threejs

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+  
+  </div>
+</template>
+
+<script>
+import * as THREE from 'three';
+
+var scene, camera, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = 500;
+
+	geometry = new THREE.BoxGeometry( 200, 200, 200 );
+	material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh ); 
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	mesh.rotation.x += 0.05;
+	mesh.rotation.y += 0.03;
+
+	renderer.render( scene, camera );
+
+}
+</script>
+
+<style>
+
+</style>
