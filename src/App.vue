f665a47c235c320be603ee3ee1ebdb9a954f9c7e
gmakter <a845354046@qq.com>
2017-08-04 15:41:08 +0800

    threejs 画线

--- src/App.vue
+++ src/App.vue
@@ -6,45 +6,41 @@
 
 <script>
 import * as THREE from 'three';
+// var THREE = require('three');
 
-var scene, camera, renderer;
-var geometry, material, mesh;
+var scene = new THREE.Scene();//生成场景
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );//生成相机
 
-init();
-animate();
+var renderer = new THREE.WebGLRenderer();//生成渲染器
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.style.margin="0px";
+document.body.style.overflow="hidden";
+renderer.domElement.style.height="100%";
+renderer.domElement.style.width="100%";
+document.body.appendChild( renderer.domElement );
 
-function init() {
+camera.position.z = 25;//摄像机Z轴距离
 
-	scene = new THREE.Scene();
+var material = new THREE.LineBasicMaterial({ color: 0xffffff });
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-25, 0, 0));
+// geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(25, 0, 0));
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = 500;
+var geometry1 = new THREE.Geometry();
+geometry1.vertices.push(new THREE.Vector3(0, 20, 0));
+geometry1.vertices.push(new THREE.Vector3(0, -20, 0));
 
-	geometry = new THREE.BoxGeometry( 200, 200, 200 );
-	material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+var line = new THREE.Line(geometry, material);
+var line1 = new THREE.Line(geometry1, material);
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh ); 
+scene.add(line);
+scene.add(line1);
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.render(scene, camera);
 
-	document.body.appendChild( renderer.domElement );
-
-}
-
-function animate() {
-
-	requestAnimationFrame( animate );
-
-	mesh.rotation.x += 0.05;
-	mesh.rotation.y += 0.03;
-
-	renderer.render( scene, camera );
-
-}
 </script>
 
 <style>
-
+html,body { overflow-y:hidden; }
 </style>
